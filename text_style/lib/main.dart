76b5d68288c6c67665b8d3ce64761143fa05e02c
felixyunianto <felixyunianto@gmail.com>
2021-06-27 04:31:18 +0700

    7. Text Style

---
+++ text_style/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home : Scaffold(
+        appBar : AppBar(title : Text("Text Style")),
+        body : Center(
+          child : Text(
+            "Ini adalah text",
+            style : TextStyle(
+              fontFamily : "CrashLandingBB",
+              fontStyle : FontStyle.italic,
+              fontSize: 30,
+              decoration : TextDecoration.overline,
+              decorationColor : Colors.red,
+              decorationThickness: 5,
+              decorationStyle: TextDecorationStyle.wavy
+            )
+          )
+        )
+      )
+    );
+  }
+}
